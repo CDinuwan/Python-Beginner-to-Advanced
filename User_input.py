57ebfc6eabf2c42f83da73eac787ae6eb3684644
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-25 16:36:08 +0530

    Practise

---
+++ User_input.py
@@ -0,0 +1,10 @@
+x = input("x: ")
+
+print(int(x))
+float(x)
+bool(x)
+
+# Flasy
+# ""
+# 0
+# []
---
+++ if_else.py
@@ -0,0 +1,7 @@
+age = 22
+if age >= 18:
+    print("Younger")
+elif age >= 13:
+    print("Teenager")
+else:
+    print("Too old")
